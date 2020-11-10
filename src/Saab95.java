679f9ad3ab1b066cb4d0feb3d25424a361a286f0
hampusedeland <hamsteylv@hotmail.com>
2020-11-10 14:26:44 +0100

    Till johani aspi

--- src/Saab95.java
+++ src/Saab95.java
@@ -23,30 +23,28 @@ public class Saab95 extends  Car {
     }
 
     public void incrementSpeed(double amount){
-        if(getCurrentSpeed()>=0 && getCurrentSpeed()<=getEnginePower()) {
+        if(getCurrentSpeed()>=0 && getCurrentSpeed()+ amount<=getEnginePower()) {
             currentSpeed = getCurrentSpeed() + speedFactor() * amount;
         }
     }
 
     public void decrementSpeed(double amount){
-        currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+        if(getCurrentSpeed() - amount >=0 && getCurrentSpeed() <=getEnginePower()) {
+            currentSpeed = getCurrentSpeed() - speedFactor() * amount;
+        }
     }
     
     // TODO fix this method according to lab pm
     public void gas(double amount){
         if(amount<=1 && amount>=0){
-            if(amount>=getCurrentSpeed()) {
                 incrementSpeed(amount);
-            }
         }
     }
 
     // TODO fix this method according to lab pm
     public void brake(double amount){
         if(amount<=1 && amount>=0) {
-            if(amount<=getCurrentSpeed()) {
                 decrementSpeed(amount);
-            }
         }
     }
     public static void main(String args[]){
@@ -54,25 +52,11 @@ public class Saab95 extends  Car {
         snabbisen.startEngine();
         snabbisen.setCurrentdirection("west");
         System.out.println(snabbisen.getNrDoors());
-
-        snabbisen.gas(0.5);
-        snabbisen.gas(0.4);
-        snabbisen.move();
-        snabbisen.move();
-        snabbisen.move();
-       snabbisen.gas(1);
-
-
-
-        snabbisen.gas(1);
-        System.out.println(snabbisen.getCurrentdirection());
-        System.out.println(snabbisen.position());
-        snabbisen.turnLeft();
-        System.out.println(snabbisen.position());
-        snabbisen.move();
-        snabbisen.move();
-        snabbisen.move();
-        System.out.println(snabbisen.position());
+        System.out.println(snabbisen.getCurrentSpeed());
+        snabbisen.gas(0.9);
+        System.out.println(snabbisen.getCurrentSpeed());
+        snabbisen.brake(0.9);
+        System.out.println(snabbisen.getCurrentSpeed());
 
 
     }
--- src/Volvo240.java
+++ src/Volvo240.java
@@ -21,46 +21,48 @@ public class Volvo240 extends Car implements Movable{
     }
 
     public void incrementSpeed(double amount){
-	    currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+        if(getCurrentSpeed()>=0 && getCurrentSpeed()+ amount<=getEnginePower()) {
+            currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount, enginePower);
+        }
     }
 
     public void decrementSpeed(double amount){
-        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+        if(getCurrentSpeed() - amount >=0 && getCurrentSpeed() <=getEnginePower()) {
+            currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount, 0);
+        }
     }
 
     // TODO fix this method according to lab pm
     public void gas(double amount){
         if(amount<=1 && amount>=0){
-            if(amount>=getCurrentSpeed()) {
                 incrementSpeed(amount);
-            }
         }
     }
 
     // TODO fix this method according to lab pm
     public void brake(double amount){
         if(amount<=1 && amount>=0) {
-            if(amount<=getCurrentSpeed()) {
                 decrementSpeed(amount);
-            }
+
         }
     }
-
-    @Override
-    public void move() {
+    public static void main(String args[]){
+        Volvo240 volvon = new Volvo240();
+        volvon.startEngine();
+        volvon.setCurrentdirection("west");
+        System.out.println(volvon.getNrDoors());
+        for(int i= 0 ; i<=200 ; i++) {
+            volvon.gas(0.9);
+        }
+        System.out.println(volvon.getCurrentSpeed());
+        for(int i= 0 ; i<=200 ; i++) {
+            volvon.brake(0.9);
+        }
+        System.out.println(volvon.getCurrentSpeed());
 
     }
 
-    @Override
-    public void turnLeft() {
 
-    }
-
-
-    @Override
-    public void turnRight() {
-
-    }
 
 
 
